135a1a7a3488e82ef1a714bc8d8db70a5241ad1c
Abby <abby_faigao@yahoo.com>
2022-01-14 23:33:08 +0000

    clean up text

--- 4_queries/teachers_name_and_number_of_assistances.sql
+++ 4_queries/teachers_name_and_number_of_assistances.sql
@@ -1,7 +1,3 @@
--- We need to know which teachers actually assisted students during any cohort, and how many assistances they did for that cohort.
-
--- Perform the same query as before, but include the number of assistances as well.
-
 SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
